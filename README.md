5eabbdeb73964ffaec62da91a3f1c58c2fdecb63
slifty <slifty@gmail.com>
2013-05-13 14:18:35 -0400

    merge

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-I can push to this repo! :D
+Gun Show
+==============
+An interactive narrative to help viewers empathetically understand the US gun debate.
 
+Floor Object
+----------------
+The convention is defined in json in the following format:
+{
+  "title": "", // The name of the convention floor
+  "booths": [ ... ] // A list of booth objects
+}
+
+Booth Object
+----------------
+{
+  "x": "", // distance from left in pixels
+  "y": "", // distance from top in pixels
+  "height": "", // height in pixels
+  "width": "", // width in pixels
+  "background_url": "", // URL for background
+  "booth_url": "", // URL to go to when booth is clicked
+}
+
+
+
+
+Code Conventions
+----------------
+
+Indent using spaces (two spaces per indent)
---
+++ public/javascripts/floor.js
@@ -0,0 +1,82 @@
+;(function ( $, window, document, undefined ) {
+
+  // Create the defaults once
+  var pluginName = "Floor",
+    defaults = {
+      booths: []
+    };
+
+  // The actual plugin constructor
+  function Plugin( el, options ) {
+    this.el = el;
+    this.$el = $(el);
+
+    // jQuery has an extend method which merges the contents of two or
+    // more objects, storing the result in the first object. The first object
+    // is generally empty as we don't want to alter the default options for
+    // future instances of the plugin
+    this.options = $.extend( {}, defaults, options );
+
+    this._defaults = defaults;
+    this._name = pluginName;
+
+    this.init();
+  }
+
+  Plugin.prototype = {
+    init: function() {
+      this.$el
+        .height(100)
+        .width(100)
+        .css("background","red")
+        .css("position", "absolute");
+      for(var x in this.options.booths) {
+        var booth = this.options.booths[x];
+        var $booth = $("<div />")
+          .data("booth", booth)
+          .css("position", "absolute")
+          .css("left", booth.x)
+          .css("top", booth.y)
+          .height(booth.height)
+          .width(booth.width)
+          .appendTo(this.$el);
+
+        booth.$el = $booth;
+      }
+    },
+
+  };
+
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[pluginName] = function ( options ) {
+    return this.each(function () {
+      if (!$.data(this, "plugin_" + pluginName)) {
+        $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+      }
+    });
+  };
+
+})( jQuery, window, document );
+
+
+// Run the map (TODO, REMOVE ME)
+$(function(){
+  var $map = $("#map");
+  $map.ConferenceMap({
+    booths: [
+      {
+        x:10,
+        y:10,
+        height:100,
+        width: 100
+      },
+      {
+        x:200,
+        y:200,
+        height:100,
+        width: 100
+      },
+    ]
+  });
+})
